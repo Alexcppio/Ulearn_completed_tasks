e7fd1bf320e7e299b1663da81cc73ffcc7f76c70
Alexcppio <62553154+Alexcppio@users.noreply.github.com>
2021-03-28 11:09:35 +0300

    Add files via upload

---
+++ DragonFractal/DragonFractalTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace Fractals
+{
+	internal static class DragonFractalTask
+	{
+		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		{
+			var x = 1.0;
+			var y = 0.0;
+			var random = new Random(seed);
+
+			for (int i = 0; i < iterationsCount; i++)
+            {
+				var nextNumber = random.Next(1, 3);
+				if(nextNumber == 1)
+                {
+					var newX = (x * Math.Cos(Math.PI * 45.0 / 180.0) - y * Math.Sin(Math.PI * 45.0 / 180.0)) / Math.Sqrt(2.0);
+					var newY = (x * Math.Sin(Math.PI * 45.0 / 180.0) + y * Math.Cos(Math.PI * 45.0 / 180.0)) / Math.Sqrt(2.0);
+					x = newX;
+					y = newY;
+				}
+				else if(nextNumber == 2)
+                {
+					var newX = (x * Math.Cos(Math.PI * 135.0 / 180.0) - y * Math.Sin(Math.PI * 135.0 / 180.0)) / Math.Sqrt(2.0) + 1;
+					var newY = (x * Math.Sin(Math.PI * 135.0 / 180.0) + y * Math.Cos(Math.PI * 135.0 / 180.0)) / Math.Sqrt(2.0);
+					x = newX;
+					y = newY;
+				}
+				pixels.SetPixel(x, y);
+			}
+		}
+	}
+}
